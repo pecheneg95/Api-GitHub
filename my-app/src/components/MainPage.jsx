e413f876408f444c1cddcd4efefdffbf913a65cf
Artsiom Piachora <pechera20@gmail.com>
2022-04-25 16:19:19 +0300

    feat: add not found page

--- my-app/src/components/MainPage.jsx
+++ my-app/src/components/MainPage.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import img from "../assets/svg/search.svg";
+import searchImg from "../assets/svg/search.svg";
+import notFoundImg from "../assets/svg/Union.svg";
 
 function MainPage(props) {
   return (
+      /* Стартовое состояние */
     <div className="main-page">
       <div class="start-message">
-        <img src={img}></img>
+        <img src={searchImg}></img>
         <p className="message">Start with searching a GitHub user</p>
       </div>
     </div>
+    
+   /* User not found 
+    <div className="main-page">
+    <div class="union-message">
+      <img src={notFoundImg}></img>
+      <p className="message">User not found</p>
+    </div>
+  </div>
+  */
   );
+  
 }
 
 export default MainPage;
